4eaee80c40440e773836b393865562ff87421a5c
luciano.vannelli <luciano.vannelli@ecloudsolutions.com>
2023-02-23 10:46:40 -0300

    download ruta

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
 const express = require("express");
 const bodyParse = require("body-parser");
 const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: function(req, file, callback) {
+    destination: function (req, file, callback) {
         callback(null, './uploads'); // ubicación donde se guardarán los archivos
     },
-    filename: function(req, file, callback) {
+    filename: function (req, file, callback) {
         callback(null, file.originalname); // nombre del archivo
     }
 });
-const upload = multer({ storage: storage, fileFilter: function (req, file, callback) {
-    if (file.mimetype !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
-        return callback(new Error('Tipo de archivo no permitido. Solo se permiten archivos de Excel.'));
+const upload = multer({
+    storage: storage, fileFilter: function (req, file, callback) {
+        if (file.mimetype !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+            return callback(new Error('Tipo de archivo no permitido. Solo se permiten archivos de Excel.'));
+        }
+        callback(null, true);
     }
-    callback(null, true);
-}});
+});
 
 const app = express();
 const PORT = process.env.PORT || 3977;
 
-app.use(bodyParse.urlencoded({ extended:true}));
+app.use(bodyParse.urlencoded({ extended: true }));
 app.use(bodyParse.json());
 
-app.get("/", (req,res) => {
-    res.status(200).send({ message: "Hola"});
+app.get("/", (req, res) => {
+    res.status(200).send({ message: "Hola" });
 });
 
-app.post("/welcome", (req,res) => {
+app.post("/welcome", (req, res) => {
     const { username } = req.body;
-    res.status(200).send({ message: `hola ${username}`});
+    res.status(200).send({ message: `hola ${username}` });
 });
 
-app.post("/api/lucho", upload.single('file') ,async (req, res) => {
-  
+app.post("/api/lucho", upload.single('file'), async (req, res) => {
+
     const file = req.file;
     if (!file) {
         const error = new Error('Por favor seleccione un archivo de Excel');
@@ -42,8 +45,18 @@ app.post("/api/lucho", upload.single('file') ,async (req, res) => {
     }
     res.send(file);
     console.log("entro");
-  });
+});
 
-app.listen(PORT , () => {
+app.get('/download', function (req, res) {
+    const filePath = path.join(__dirname, './uploads/redpack.xlsx');
+    res.download(filePath, function (err) {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error al descargar el archivo');
+        }
+    });
+});
+
+app.listen(PORT, () => {
     console.log(`server listo puerto ${PORT}`)
 })
